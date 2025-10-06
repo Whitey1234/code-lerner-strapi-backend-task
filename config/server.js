d3c6d63943e2924dcb6e29fe4bba756e99f04a98
najmul <n.siyam120@gmail.com>
2025-10-06 16:25:20 +0600

    fixk url

--- config/server.js
+++ config/server.js
@@ -2,8 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://code-lerner-strapi-backend-task-production.up.railway.app/'),
+  proxy: env.bool('IS_PROXY', true),
   app: {
     keys: env.array('APP_KEYS'),
+    proxy: true,
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
